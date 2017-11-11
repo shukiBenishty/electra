8fb4276a34f1e021a6c8c23ebe6234491c0e6839
Oleg Kleiman <oleg_kleyman@hotmail.com>
2017-11-11 22:07:34 +0200

    Initial commit

---
+++ app/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from '../css/header.css';
+
+class Header extends React.Component {
+
+  render() {
+    return (<div>Header</div>);
+  }
+
+};
+
+export default Header;
